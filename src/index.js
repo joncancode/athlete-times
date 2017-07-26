ac560ead0abc446d49febb3592892ce86fcf2b34
zwillerzoog <marsh.heidi.c@gmail.com>
2017-07-26 18:37:12 -0400

    mentor session

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log('class', <App />)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
